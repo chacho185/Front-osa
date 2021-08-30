0c113f588634e734777b3c37784a642035e9717b
Miloš Sikimić <sikimic10@gmail.com>
2021-08-30 13:05:54 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div id="app">
+    <Navigation />
+    <router-view></router-view>
+  
+  </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
+import Navigation from './components/navigation/TheNavigation.vue';
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Navigation
   }
 }
 </script>
---
+++ src/router/index.js
@@ -0,0 +1,8 @@
+import { createRouter, createWebHistory} from 'vue-router'
+
+const router = new createRouter({
+    history: createWebHistory(),
+    routes:[]
+})
+
+export default router;
